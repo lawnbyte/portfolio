113b430350c4908ccba7763e908400c268f57217
trungpq287@gmail.com <trungpq287@gmail.com>
2025-06-13 15:08:54 +0700

    refactor: update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,20 @@
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata = {
-  title: "Lama Dev Portfolio App",
-  description: "The best animated portfolio page",
+    title: "Lawnbyte",
+    description: "Lawnbyte",
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+        <body className={inter.className}>
         <TransitionProvider>{children}</TransitionProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -1,16 +1,16 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import {usePathname} from "next/navigation";
 
-const NavLink = ({ link }) => {
-  const pathName = usePathname();
+const NavLink = ({link}) => {
+    const pathName = usePathname();
 
-  return (
-    <Link className={`rounded p-1 ${pathName === link.url && "bg-black text-white"}`} href={link.url}>
-      {link.title}
-    </Link>
-  );
+    return (
+        <Link className={`rounded p-1 ${pathName === link.url && "bg-black text-white"}`} href={link.url}>
+            {link.title}
+        </Link>
+    );
 };
 
 export default NavLink;
